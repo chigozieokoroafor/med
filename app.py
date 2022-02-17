e836ef368a24a5eb1c7a2450de96ae3924e8cfb2
chigozieokoroafor <48325147+chigozieokoroafor@users.noreply.github.com>
2022-02-16 22:03:35 -0800

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from flask_bcrypt import Bcrypt
 
 
 #mongo = pymongo.MongoClient(host="localhost", port=27017, serverSelectionTimeoutMS=100000)
-mongo = pymongo.MongoClient("mongodb+srv://aptcoder:wonderful.@cluster0.5gyf4.mongodb.net/swep-be?retryWrites=true")
+mongo = pymongo.MongoClient(connection_string)
 mongo.server_info()
 db = mongo.get_database("swep-be")
 user_check = db.stage_one_vps
